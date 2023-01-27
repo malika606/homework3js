03e758d80826c6af41b697b43c811d218de03a23
Malika <sirojiddinovamalika06@gmail.com>
2023-01-27 20:23:30 +0500

    full project

---
+++ script.js
@@ -0,0 +1,7 @@
+var num1 = +prompt(' Введите основание для степени ');
+var num2 = +prompt(' Введите показатель степени ');
+var str = 1;
+for (i = 0; i < num2; i++){
+    console.log(str = str*num1);
+}
+alert(str);
